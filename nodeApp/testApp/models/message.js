2686b0b3c956baf7d0849a54433be49a3c2c80a9
Nattapon Benz <sevipsagis@gmail.com>
2018-09-27 20:14:15 +0700

    add nodeApp template

---
+++ nodeApp/testApp/models/message.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+// Schema Validation
+var messageSchema = mongoose.Schema({
+    room: {type: String, required: true},
+    elec_usage: {type: Number, required: true},
+    water_usage: {type: Number, required: true},
+    created_date: {type:Date, default: Date.now}
+},{
+    versionKey: false 
+});
+
+var Message = mongoose.model('data_from_mqtt', messageSchema);
+module.exports = Message;
